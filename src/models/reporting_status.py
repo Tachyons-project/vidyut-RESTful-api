a0409ff4b50f5cdd35cfca5b2050a7cc975cacfd
jyrj <jayarajevur@gmail.com>
2020-08-09 02:07:51 +0530

    your message

---
+++ src/models/reporting_status.py
@@ -0,0 +1,57 @@
+c/models/Reporting_Status.py
+from marshmallow import fields, Schema
+import datetime
+from . import db
+
+class Reporting_Status(db.Model):
+  """
+  User Model
+  """
+
+  # table name
+  __tablename__ = 'users'
+
+  id = db.Column(db.Integer, primary_key=True)
+  first_name = db.Column(db.String(128), nullable=False)
+  last_name = db.Column(db.String(128), nullable=False)
+  email = db.Column(db.String(128), unique=True, nullable=False)
+  password = db.Column(db.String(128), nullable=True)
+  created_at = db.Column(db.DateTime)
+  reporting_update = db.Column(db.DateTime)
+
+  # class constructor
+  def __init__(self, data):
+    """
+    Class constructor
+    """
+    self.name = data.get('name')
+    self.email = data.get('email')
+    self.password = data.get('password')
+    self.created_at = datetime.datetime.utcnow()
+    self.modified_at = datetime.datetime.utcnow()
+
+  def save(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self, data):
+    for key, item in data.items():
+      setattr(self, key, item)
+    self.modified_at = datetime.datetime.utcnow()
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  @staticmethod
+  def get_all_users():
+    return Reporting_Status.query.all()
+
+  @staticmethod
+  def get_one_user(id):
+    return Reporting_Status.query.get(id)
+
+  
+  def __repr(self):
+    return '<id {}>'.format(self.id)
